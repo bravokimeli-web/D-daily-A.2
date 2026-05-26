8c05f3a6cf0cbbcf3cf3a7c25d7c544521c5e997
bravokimeli-web <bravokimeli@gmail.com>
2026-05-26 19:29:48 +0300

    added

--- next.config.ts
+++ next.config.ts
@@ -40,11 +40,26 @@ const nextConfig: NextConfig = {
   async headers() {
     return [
       {
+        source: "/_next/static/:path*",
+        headers: [
+          {
+            key: "Cache-Control",
+            value: "public, max-age=31536000, immutable",
+          },
+        ],
+      },
+      {
         source: "/(.*)",
         headers: [
+          {
+            key: "Cache-Control",
+            value: "no-cache, no-store, must-revalidate, public, max-age=0",
+          },
           { key: "X-DNS-Prefetch-Control", value: "on" },
           { key: "X-Content-Type-Options", value: "nosniff" },
           { key: "Referrer-Policy", value: "strict-origin-when-cross-origin" },
+          { key: "Pragma", value: "no-cache" },
+          { key: "Expires", value: "0" },
         ],
       },
     ];
